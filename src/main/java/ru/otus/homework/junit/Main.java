5e6b8a021d3f8a70bd33ee5122bb863b0dffad0b
bla <bychyby@mail.ru>
2025-01-30 19:18:34 +0300

    init

---
+++ src/main/java/ru/otus/homework/junit/Main.java
@@ -0,0 +1,10 @@
+package ru.otus.homework.junit;
+
+
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+    }
+}
